08c85da61b9d6770a803406ce6ed85a4d10c7d22
Jazzzy <ruben@osor.io>
2018-02-10 15:19:27 +0000

    Added dynamic embedding of shaders into precompiled headers, supported all common shader types

--- VR_ButNotReally/src/render/shaders/compile_shaders.bat
+++ VR_ButNotReally/src/render/shaders/compile_shaders.bat
@@ -1,5 +1,21 @@
-C:\dep\VulkanSDK\1.0.65.1\Bin\glslangValidator.exe -V triangle.vert -o ..\..\res\shaders\triangle_vert.spv
-C:\dev\VulkanRenderer\BinaryToCpp\bin\x64\Release\BinaryToCpp.exe ..\..\res\shaders\triangle_vert.spv triangle_vert > triangle_vert.hpp
+echo.
+echo Compiling and embedding shaders...
 
-C:\dep\VulkanSDK\1.0.65.1\Bin\glslangValidator.exe -V triangle.frag -o ..\..\res\shaders\triangle_frag.spv
-C:\dev\VulkanRenderer\BinaryToCpp\bin\x64\Release\BinaryToCpp.exe ..\..\res\shaders\triangle_frag.spv triangle_frag > triangle_frag.hpp
+
+@echo off
+	set types=vert tesc tese geom frag conf
+	(for %%t in (%types%) do (  
+		for /R "./" %%f in (*.%%t) do (
+
+			C:\dep\VulkanSDK\1.0.65.1\Bin\glslangValidator.exe -V %%f -o %%~nf_%%t.spv
+			C:\dev\VulkanRenderer\BinaryToCpp\bin\x64\Release\BinaryToCpp.exe %%~nf_%%t.spv %%~nf_%%t > %%~nf_%%t.hpp
+			del %%~nf_%%t.spv
+
+			echo "Compiled and embedded %%t shader: %%~nf"
+		)
+	))
+
+
+echo Finished compilation and embedding of shaders
+echo.
+echo.
